7236f02c8a819c4af186838b104c70072f8c1223
elijhonathan <eligrancej@hotmail.com>
2023-10-23 19:36:00 -0300

    programa rodando

--- Models/Iphone.cs
+++ Models/Iphone.cs
@@ -1,8 +1,18 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Iphone
+    public class Iphone : Smartphone
     {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+            {
+                //Construtor da classe base é chamado
+            }
+
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
+        public override void InstalarAplicativo(string nomeApp)
+            {
+                Console.WriteLine($"Instalando o aplicativo {nomeApp} no Iphone.");
+            }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Smartphone Nokia");
+        Nokia nokia = new Nokia(numero: "99999-5552", modelo: "Nokia 2200", imei: "123456789", memoria: 5);
+        nokia.Ligar();
+        nokia.InstalarAplicativo("Game Snake II");
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Smartphone Apple");
+        Iphone iphone = new Iphone(numero: "99855-3325", modelo: "Iphone 15", imei: "987654321", memoria: 128);
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Game Mortal Kombat 1");
+    }
+}
+
